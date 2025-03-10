565c40b371bba9fb72453fe77feba84382bd1b0a
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-03-10 18:40:48 +0200

    delete unused

--- src/components/BlueVert.jsx
+++
@@ -1,9 +0,0 @@
-// import styles from '../styles/bluevert.module.css';
-
-export default function BlueVert(props) {
-  return (
-    <div
-      className={`bg-(--color-foreground) w-xl sticky top-0 -z-100 ${props.height} justify-self-center`}
-    />
-  );
-}
--- src/components/LeftLine.jsx
+++
@@ -1,5 +0,0 @@
-export default function LeftLine() {
-  return (
-    <div className="fixed left-2 -z-11 h-dvh border-l-1 border-solid border-(--color-foreground)"></div>
-  );
-}
