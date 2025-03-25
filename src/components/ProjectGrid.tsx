abecdb3e8c62e2f00e7f5cd5e0f6efba20300951
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-03-25 13:09:23 +0200

    add filter to projects page

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -1,16 +1,8 @@
-import {
-  AnimatePresence,
-  motion,
-  useMotionValueEvent,
-  useScroll,
-  useSpring,
-} from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import type { OstDocument } from 'outstatic';
 import styles from '../styles/projects.module.css';
-
-// import BlueAbsolute from './BlueAbsolute';
 
 type Item = {
   tags?: { value: string; label: string }[];
@@ -31,83 +23,63 @@ const ProjectGrid = ({
 }: Props) => {
   return (
     <div className="relative">
-      {/* <BlueAbsolute /> */}
-      {/* <BlueVertGrid height={'h-screen'} /> */}
       <section id={collection}>
-        {/* <h2 className="mb-8 text-5xl md:text-6xl font-bold tracking-tighter leading-tight">
-        {title}
-      </h2> */}
         <div className="grid grid-cols-1 mx-4 lg:mx-0 sm:grid-cols-2 xl:grid-cols-3 sm:gap-x-6 lg:gap-x-8 gap-y-4 sm:gap-y-6 lg:gap-y-8 pt-8">
           {items.map((item, id) => (
             <AnimatePresence mode="popLayout">
               <motion.div
                 key={item.slug}
-                href={`/${collection}/${item.slug}`}
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0 }}
-                // style={box}
-                // key="box"
               >
-                <div
-                  className={`${styles.projectLink} cursor-pointer project-card md:w-full scale-100 hover:scale-[1.02] active:scale-[0.97] origin-top-left motion-safe:transform-gpu transition-all duration-100 motion-reduce:hover:scale-100 overflow-hidden sm:shadow-[6px_6px_0_var(--color-background),12px_12px_0_var(--color-foreground)] sm:hover:shadow-[-6px_-6px_0_var(--color-background),-12px_-12px_0_var(--color-foreground)]`}
-                >
-                  <div className={styles.gridItem}>
-                    <Image
-                      src={item.coverImage ?? ''}
-                      alt={`Cover Image for ${item.title}`}
-                      className={`${styles.projectImage} object-cover object-center w-full h-auto`}
-                      width={0}
-                      height={0}
-                      sizes="(min-width: 768px) 347px, 192px"
-                      priority={priority && id <= 2}
-                    />
-                    {collection === 'projects' && (
-                      <div className="text-white absolute top-3 left-3 -translate-y-0 -translate-x-0">
-                        <h2
-                          className={`${styles.projectTitle} p-2 bg-opacity-0 text-left whitespace-normal font-bold tracking-wide text-4xl uppercase  leading-12`}
-                        >
-                          {item.title}
-                        </h2>
-                        <h3
-                          className={`${styles.projectDescription} pl-2 pr-6 bg-opacity-0 text-left whitespace-normal font-medium tracking-wide text-md`}
-                        >
-                          {item.description}
-                        </h3>
-                      </div>
-                    )}
-                    {collection === 'projects' && (
-                      <div
-                        className={`${styles.projectTags} absolute bottom-4 left-5 translate-y-0 -translate-x-0`}
-                      >
-                        {Array.isArray(item?.projectTags)
-                          ? item.projectTags.map(({ label }) => (
-                              <span
-                                key={label}
-                                className="uppercase inline-block bg-white rounded-full px-3 py-1 text-md font-semibold text-((gray-700) mr-2 mb-2)"
-                              >
-                                {label}
-                              </span>
-                            ))
-                          : null}
-                      </div>
-                    )}
-                  </div>
-                  {/* {collection === 'projects' && (
-                  <div className="p-4">
-                    {Array.isArray(item?.projectTags)
-                      ? item.projectTags.map(({ label }) => (
-                          <span
-                            key={label}
-                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                <Link href={`/${collection}/${item.slug}`}>
+                  <div
+                    className={`${styles.projectLink} cursor-pointer project-card md:w-full scale-100 hover:scale-[1.02] active:scale-[0.97] origin-top-left motion-safe:transform-gpu transition-all duration-100 motion-reduce:hover:scale-100 overflow-hidden sm:shadow-[6px_6px_0_var(--color-background),12px_12px_0_var(--color-foreground)] sm:hover:shadow-[-6px_-6px_0_var(--color-background),-12px_-12px_0_var(--color-foreground)]`}
+                  >
+                    <div className={styles.gridItem}>
+                      <Image
+                        src={item.coverImage ?? ''}
+                        alt={`Cover Image for ${item.title}`}
+                        className={`${styles.projectImage} object-cover object-center w-full h-auto`}
+                        width={0}
+                        height={0}
+                        sizes="(min-width: 768px) 347px, 192px"
+                        priority={priority && id <= 2}
+                      />
+                      {collection === 'projects' && (
+                        <div className="text-white absolute top-3 left-3 -translate-y-0 -translate-x-0">
+                          <h2
+                            className={`${styles.projectTitle} p-2 bg-opacity-0 text-left whitespace-normal font-bold tracking-wide text-4xl uppercase  leading-12`}
                           >
-                            {label}
-                          </span>
-                        ))
-                      : null}
+                            {item.title}
+                          </h2>
+                          <h3
+                            className={`${styles.projectDescription} pl-2 pr-6 bg-opacity-0 text-left whitespace-normal font-medium tracking-wide text-md`}
+                          >
+                            {item.description}
+                          </h3>
+                        </div>
+                      )}
+                      {collection === 'projects' && (
+                        <div
+                          className={`${styles.projectTags} absolute bottom-4 left-5 translate-y-0 -translate-x-0`}
+                        >
+                          {Array.isArray(item?.projectTags)
+                            ? item.projectTags.map(({ label }) => (
+                                <span
+                                  key={label}
+                                  className="uppercase inline-block bg-white rounded-full px-3 py-1 text-md font-semibold text-((gray-700) mr-2 mb-2)"
+                                >
+                                  {label}
+                                </span>
+                              ))
+                            : null}
+                        </div>
+                      )}
+                    </div>
                   </div>
-                )} */}
-                </div>
+                </Link>
               </motion.div>
             </AnimatePresence>
           ))}
---
+++ src/components/TagsFilter.jsx
@@ -0,0 +1,59 @@
+'use client';
+import { motion } from 'motion/react';
+import { useState } from 'react';
+import ProjectGrid from './ProjectGrid';
+
+const TagsFilter = ({ allProjects }) => {
+  const [tagFilter, setTagFilter] = useState('all');
+  let filteredProjects = allProjects;
+  if (tagFilter === 'all') {
+    filteredProjects = allProjects;
+  } else
+    filteredProjects = allProjects.filter((project) => {
+      let selected = project.projectTags.some(({ value }) =>
+        value.includes(`${tagFilter}`),
+      );
+      return selected;
+    });
+  const TagItem = (props) => {
+    return (
+      <motion.button
+        className={`${
+          props.tagValue === tagFilter
+            ? `bg-(--color-vivid) text-black`
+            : 'bg-(--color-foreground)'
+        } uppercase inline-block rounded-full px-3 py-1 text-md font-medium cursor-pointer scale-100 hover:scale-[1.15] hover:mx-4 active:scale-[0.97] transition-all tracking-wide shadow-[0px_0px_0px_2px_var(--color-background),0px_0px_0px_5px_var(--color-foreground),0px_0px_0px_6px_var(--color-vivid)]`}
+        formAction={() => setTagFilter(props.tagValue)}
+      >
+        {props.tagLabel}
+      </motion.button>
+    );
+  };
+
+  return (
+    <div className="relative ">
+      <section>
+        <form className="flex mt-10 flex-wrap gap-5 lg:gap-4 mx-4 lg:mx-0 text-white ">
+          <TagItem tagLabel="All" tagValue="all" />
+          <TagItem tagLabel="Code" tagValue="code" />
+          <TagItem tagLabel="Sound Design" tagValue="soundDesign" />
+          <TagItem tagLabel="Music Videos" tagValue="musicVideo" />
+          <TagItem tagLabel="Albums" tagValue="album" />
+          <TagItem tagLabel="Soundtracks" tagValue="soundtrack" />
+          <TagItem tagLabel="Audio Post" tagValue="audioPost" />
+          <TagItem tagLabel="Short Films" tagValue="shortFilm" />
+          <TagItem tagLabel="Graphic Design" tagValue="design" />
+        </form>
+      </section>
+      {allProjects.length > 0 && (
+        <ProjectGrid
+          title="Projects"
+          items={filteredProjects}
+          collection="projects"
+        />
+      )}
+    </div>
+  );
+};
+
+export default TagsFilter;
