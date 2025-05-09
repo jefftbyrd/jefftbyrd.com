b3ba9d27a427428c251cddf9f38d32f61e4f334d
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-05-09 11:31:08 +0300

    update homepage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,54 +5,12 @@ import {
   useScroll,
   useSpring,
 } from 'motion/react';
-// import { absoluteUrl } from '@/lib/utils';
-// import { Metadata } from 'next';
-// import { load } from 'outstatic/server';
 import HomeFeaturedProject from '../components/HomeFeaturedProject';
 import HomeMenuItem from '../components/HomeMenuItem';
 import Layout from '../components/Layout';
 import Logo from '../components/Logo';
 
-// import markdownToHtml from '../lib/markdownToHtml';
-
-// export async function generateMetadata(): Promise<Metadata> {
-//   const { page } = await getData();
-//   /* @next-codemod-error 'params' is passed as an argument. Any asynchronous properties of 'props' must be awaited when accessed. */
-
-//   if (!page) {
-//     return {};
-//   }
-
-//   return {
-//     title: page.title + ` | Jeff T Byrd`,
-//     description: page.description,
-//     openGraph: {
-//       title: page.title,
-//       description: page.description,
-//       type: 'article',
-//       url: absoluteUrl(`/${page.slug}`),
-//       images: [
-//         {
-//           url: absoluteUrl(page?.coverImage || '/images/jefftbyrd.png'),
-//           width: 1200,
-//           height: 630,
-//           alt: page.title,
-//         },
-//       ],
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: page.title,
-//       description: page.description,
-//       images: absoluteUrl(page?.coverImage || '/images/jefftbyrd.png'),
-//     },
-//   };
-// }
-
 export default async function Index() {
-  // const { page, content } = await getData();
-  // const { content } = await getData();
-
   return (
     <Layout>
       <div className="max-w-none mx-auto px-0 grid">
@@ -97,43 +55,3 @@ export default async function Index() {
     </Layout>
   );
 }
-
-// async function getData() {
-//   const db = await load();
-
-//   // const page = await db
-//   //   .find({ collection: 'pages', slug: 'home' }, [
-//   //     'content',
-//   //     'title',
-//   //     'slug',
-//   //     'coverImage',
-//   //     'description',
-//   //   ])
-//   //   .first();
-
-//   // const content = await markdownToHtml(page.content);
-
-//   //   const allPosts = await db
-//   //     .find({ collection: 'posts' }, [
-//   //       'title',
-//   //       'publishedAt',
-//   //       'slug',
-//   //       'coverImage',
-//   //       'description',
-//   //       'tags',
-//   //     ])
-//   //     .sort({ publishedAt: -1 })
-//   //     .toArray();
-
-//   //   const allProjects = await db
-//   //     .find({ collection: 'projects' }, ['title', 'slug', 'coverImage'])
-//   //     .sort({ publishedAt: -1 })
-//   //     .toArray();
-
-//   return {
-//     // page,
-//     // content,
-//     // allPosts,
-//     // allProjects,
-//   };
-// }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,22 +5,15 @@ import {
   useScroll,
   useSpring,
 } from 'motion/react';
-import { useRef, useState } from 'react';
 import styles from '../styles/home.module.css';
 
 export default function Logo() {
-  // const ref = useRef(null);
-  // const { scrollYProgress } = useScroll({
-  //   // target: ref,
-  //   offset: ['end end', 'start start'],
-  // });
-
   return (
     <>
       <motion.h1
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        className={`py-2 sm:py-0 lg:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} z-100`}
+        className={`py-2 sm:py-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} z-100`}
       >
         Jeff <span className={styles.bump}>T</span> Byrd
       </motion.h1>
@@ -28,21 +21,10 @@ export default function Logo() {
       <motion.h2
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className={`${styles.subtitle} text-center uppercase z-10 mb-2 md:mb-0 text-2xl md:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))]`}
+        className={`${styles.subtitle} text-center uppercase z-10 mb-2 md:mb-0 text-xl md:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto`}
       >
         Web developer, composer & sound designer
       </motion.h2>
     </>
-
-    // <motion.h1
-    //   // initial={{ scale: 1 }}
-    //   // whileInView={{ opacity: 1 }}
-    //   // viewport={{ root: scrollRef }}
-    //   // ref={ref}
-    //   className={styles.logo}
-    //   // style={{ scale: scrollYProgress }}
-    // >
-    //   Jeff <span className={styles.bump}>T</span> Byrd
-    // </motion.h1>
   );
 }
