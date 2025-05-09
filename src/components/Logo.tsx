ab50b1e85e6d05514bcd19315c2f49cd19cd32a3
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-05-09 11:35:08 +0300

    update homepage

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,7 +13,7 @@ export default function Logo() {
       <motion.h1
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        className={`py-2 sm:py-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} z-100`}
+        className={`pt-2 sm:pt-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} z-100`}
       >
         Jeff <span className={styles.bump}>T</span> Byrd
       </motion.h1>
@@ -21,7 +21,7 @@ export default function Logo() {
       <motion.h2
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className={`${styles.subtitle} text-center uppercase z-10 mb-2 md:mb-0 text-xl md:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto`}
+        className={`${styles.subtitle} text-center uppercase z-10 mb-2 md:mb-0 text-xl sm:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto`}
       >
         Web developer, composer & sound designer
       </motion.h2>
