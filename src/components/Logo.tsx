95f304b7afa075f91af5094793bc59f7c512860a
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-01-16 10:05:52 +0200

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,7 +13,7 @@ export default function Logo() {
       <motion.h1
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        className={`pt-2 sm:pt-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} drop-shadow-[0px_0px_0.4rem_#ca2bc4]`}
+        className={`pt-2 sm:pt-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} drop-shadow-[0px_0px_0.4rem_#ca2bc4] `}
       >
         Jeff{' '}
         <motion.span
@@ -36,9 +36,13 @@ export default function Logo() {
       <motion.h2
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className={`${styles.subtitle} text-center tracking-wide uppercase z-10 mb-2 md:mb-0 text-xl sm:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto drop-shadow-[0px_0px_0.3rem_#ca2bc4] md:leading-8 lg:leading-9 xl:leading-11 2xl:leading-13`}
+        className={`${styles.subtitle} text-center  tracking-wide uppercase z-10 mb-2 md:mb-0 text-xl sm:text-[calc(16px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto drop-shadow-[0px_0px_0.3rem_#ca2bc4] md:leading-8 lg:leading-9 xl:leading-11 2xl:leading-13`}
       >
-        <span className={`font-medium`}>Composer</span>{' '}
+        <span
+          className={`font-medium text-shadow-[3px_3px_0px_rgb(255_84_253_/_1)]`}
+        >
+          Composer
+        </span>{' '}
         <span className={` text-lg align-middle`}>
           <a href="/projects/white-lies/" className={`hover:text-white`}>
             &rarr;{' '}
@@ -48,7 +52,11 @@ export default function Logo() {
           </a>
         </span>
         <br />
-        <span className={`font-medium`}>Web Developer</span>
+        <span
+          className={`font-medium text-shadow-[3px_3px_0px_rgb(255_84_253_/_1)]`}
+        >
+          Web Developer
+        </span>
       </motion.h2>
     </>
   );
