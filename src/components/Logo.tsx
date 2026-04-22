59493de6d55ace07389b66fcd72e362e89084765
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-04-22 09:47:46 +0300

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,11 +9,11 @@ import styles from '../styles/home.module.css';
 
 export default function Logo() {
   return (
-    <>
+    <div>
       <motion.h1
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        className={`pt-2 sm:pt-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo} drop-shadow-[0px_0px_0.4rem_#ca2bc4] `}
+        className={`pt-2 sm:pt-0 sm:text-[calc(24px+(200-24)*((100vw-300px)/(1600-300)))] text-[60px] uppercase ${styles.logo}  `}
       >
         Jeff{' '}
         <motion.span
@@ -36,19 +36,10 @@ export default function Logo() {
       <motion.h2
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className={` text-center uppercase font-light z-10 mb-2 md:mb-0 text-xl sm:text-[calc(22px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto drop-shadow-[0px_0px_0.3rem_#ca2bc4] -mt-2`}
+        className={` text-center uppercase font-light z-10 mb-2 md:mb-0 text-xl sm:text-[calc(22px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto  -mt-2`}
       >
-        Composer | Web Developer
+        Composer | Sound Designer
       </motion.h2>
-      {/* <motion.div
-        className="text-center workAreas"
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0 }}
-      >
-        <div className="left">Narrative Audio</div>
-        <div className="right">Interactive Media</div>
-      </motion.div> */}
-    </>
+    </div>
   );
 }
