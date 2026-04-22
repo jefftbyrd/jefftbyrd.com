b1d02d58e2aba6f285db20f0919481f5bf326247
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-04-22 09:47:12 +0300

    add collaborate section to landing page

---
+++ src/components/Collaborate.jsx
@@ -0,0 +1,37 @@
+export default function Collaborate() {
+  return (
+    <div className="w-full h-full m-0 col-span-3 lg:pr-12 bg-white/40 p-4 lg:p-12 border border-foreground">
+      <h2 className="">Collaborate</h2>
+      <div className="gap-4 flex flex-col">
+        <h3 className="">
+          I work with producers, editors, and directors on documentary, podcast,
+          and narrative audio — from original score to full sonic identity.
+        </h3>
+
+        {/* <p>
+          My approach is rooted in narrative — understanding pacing, tone, and
+          when music should lead or step back.
+        </p> */}
+
+        <h4 className="inline font-semibold">
+          Available for remote collaboration worldwide.
+        </h4>
+
+        <div>
+          {/* <h3 className="uppercase tracking-wide font-semibold! underline underline-offset-5 decoration-2">
+            Services include:
+          </h3> */}
+          {/* <ul className="list-disc ml-4">
+            <li>
+              Original music for podcasts, films, documentaries, and other
+              narrative media
+            </li>
+            <li>Sound design and sonic world-building</li>
+            <li>Full audio identity for narrative series</li>
+          </ul> */}
+        </div>
+      </div>
+    </div>
+  );
+  p;
+}
