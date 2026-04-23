248457ace1d905695680494c3fd5bc01460d2e33
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-04-23 20:17:44 +0300

    update blueAbsolute

--- src/components/BlueAbsolute.tsx
+++ src/components/BlueAbsolute.tsx
@@ -1,10 +1,9 @@
 export default function BlueAbsolute() {
   return (
     <div className="fixed w-full left-0 -z-11 h-dvh">
-      <div className="bg-(--color-foreground) lg:bg-transparent grid lg:grid-cols-2 gap-30 ml-0 lg:mr-24 py-0 h-full -z-10 w-9/10 lg:w-auto">
+      <div className="bg-foreground lg:bg-transparent grid lg:grid-cols-2 gap-30 ml-0 lg:mr-24 py-0 h-full -z-10 mr-16 lg:w-auto">
         <div className=""></div>
-        {/* <div className={`bg-(--color-foreground)`} /> */}
-        <div className={` sm:bg-(--color-foreground)`} />
+        <div className={` sm:bg-foreground`} />
       </div>
     </div>
   );
