3d268e157b5c872c7743d4f8c8ab7a7199440f21
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-04-29 08:44:10 +0300

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -38,7 +38,7 @@ export default function Logo() {
         animate={{ opacity: 1 }}
         className={` text-center uppercase font-light z-10 mb-2 md:mb-0 text-xl sm:text-[calc(22px+(38-16)*((100vw-300px)/(1600-300)))] w-4/5 md:w-auto mx-auto  -mt-2`}
       >
-        Composer | Sound Designer
+        Composer & Sound Designer
       </motion.h2>
     </div>
   );
