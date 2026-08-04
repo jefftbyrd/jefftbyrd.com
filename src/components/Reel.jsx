c0581163e821a7be09ef653c95b8dee978437646
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-08-04 10:24:31 +0300

    add background to reel

--- src/components/Reel.jsx
+++ src/components/Reel.jsx
@@ -1,9 +1,17 @@
 'use client';
+import Image from 'next/image';
 import ReelModule from './ReelModule';
 
 export default function Reel() {
   return (
-    <div className="w-full m-0 p-0 border border-foreground group cursor-pointer bg-white/40 hover:bg-white/60 transition-colors duration-200">
+    <div className="w-full m-0 p-0 border border-foreground group cursor-pointer bg-white/20 hover:bg-white/40 transition-colors duration-200 relative">
+      <Image
+        alt="White Lies"
+        src="/images/white-lies-bloody-sunday.webp"
+        fill
+        className="object-cover mix-blend-overlay opacity-50 object-top"
+        priority
+      />
       <div className="p-4 lg:pl-8 lg:pt-7 absolute">
         <h2 className="text-2xl! lg:text-4xl! mb-1!">Narrative Podcast Reel</h2>
         <h3>
