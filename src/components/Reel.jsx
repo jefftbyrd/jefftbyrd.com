a33d1e39ea5c9fb5f01360bd33aee18b56348216
Jeff T Byrd <jeff.t.byrd@gmail.com>
2026-08-04 10:59:56 +0300

    adjust reel style

--- src/components/Reel.jsx
+++ src/components/Reel.jsx
@@ -4,12 +4,12 @@ import ReelModule from './ReelModule';
 
 export default function Reel() {
   return (
-    <div className="w-full m-0 p-0 border border-foreground group cursor-pointer bg-white/20 hover:bg-white/40 transition-colors duration-200 relative">
+    <div className="w-full m-0 p-0 border-0 border-foreground group cursor-pointer bg-white/20 hover:bg-white/40 transition-colors duration-200 relative">
       <Image
         alt="White Lies"
         src="/images/white-lies-bloody-sunday.webp"
         fill
-        className="object-cover mix-blend-overlay opacity-50 object-top"
+        className="object-cover mix-blend-overlay opacity-60 object-top"
         priority
       />
       <div className="p-4 lg:pl-8 lg:pt-7 absolute">
